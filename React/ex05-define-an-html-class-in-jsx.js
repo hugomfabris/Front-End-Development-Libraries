aa036e287e5246c1cb6f9599188cdc62229b1d9e
Hugo Fabris <hugomfabris@gmail.com>
2021-06-22 22:32:08 -0300

    React - Second commit

---
+++ React/ex05-define-an-html-class-in-jsx.js
@@ -0,0 +1,9 @@
+/*
+In JSX is that you can no longer use the word class to define HTML classes. This is because class is a reserved word in JavaScript. Instead, JSX uses className.
+*/
+
+const JSX = (
+  <div className="myDiv">
+    <h1>Add a class to this div</h1>
+  </div>
+);
---
+++ React/ex07-create-a-stateless-functional-component.js
@@ -0,0 +1,14 @@
+/*
+For now, think of a stateless component as one that can receive data and render it, but does not manage or track changes to that data.
+*/
+
+const MyComponent = function() {
+  // Change code below this line
+  return ( 
+  <div className="component">
+    This is a div
+  </div>
+  );
+
+  // Change code above this line
+}
